7a336effc84fbfcb994d5164c4db980586b78c9c
Alexsey Loginov <aleniklog@gmail.com>
2021-12-21 00:23:35 +0300

    add collection vc and end lesson 3

---
+++ Weather/VC/Weather/WeatherCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCell.swift
+//  Weather
+//
+//  Created by Алексей Логинов on 20.12.2021.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    
+    @IBOutlet weak var weather: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var time: UILabel!
+}
